9524ea9566c7d9242e42996fa421d25655bd8b91
RaquelMartins <rccms@iscte-iul.pt>
2017-12-07 15:50:54 +0000

    Added EditWindow class with everything more structured.

---
+++ src/antiSpamFilter/EditWindow.java
@@ -0,0 +1,110 @@
+package antiSpamFilter;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class EditWindow {
+	
+	
+	
+	private ArrayList<String> rulesList;
+	private ArrayList<String> weightsList;
+	
+	
+	
+	/***
+	 * This constructor builds a new Edit Window. This window will allow you to change things in the file rules.cf.
+	 * You can change the weights of the rules that you want. 
+	 * 
+	 * @author rccms-iscteiul
+	 */
+	public EditWindow() {
+		build();
+		
+	}
+	
+	
+	/***
+	 * This method builds the frame with 2 JComboBox (one for the rules and one for the weights) and one Apply button. 
+	 */
+	private void build() {
+		
+	JFrame f = new JFrame("Change rules.cf");
+	f.setLayout(new GridLayout(2,1));
+	Gui.getInstance().center(f);
+	f.setSize(380, 80);
+	JPanel p = new JPanel(new BorderLayout());
+	JComboBox<Object> rulesComboBox = new JComboBox<>();
+	JComboBox<Object> weightsComboBox = new JComboBox<>();
+	JButton applyButton = new JButton("Apply");
+	p.add(rulesComboBox,BorderLayout.CENTER);
+	p.add(weightsComboBox,BorderLayout.EAST);
+	f.add(p);
+	f.add(applyButton);
+	
+	takeCareOfRules();
+	takeCareOfWeights();
+	
+	rulesComboBox.setModel(new DefaultComboBoxModel<>(rulesList.toArray()));
+	weightsComboBox.setModel(new DefaultComboBoxModel<>(weightsList.toArray()));
+	
+	rulesComboBox.addActionListener(new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			System.out.println(rulesComboBox.getSelectedItem().toString());
+		}
+	});
+	
+	f.setVisible(true);
+	}
+	
+	
+	private void takeCareOfRules (){
+	
+		File file = new File(Gui.getInstance().getRulesPath());
+		
+		try {
+			
+			rulesList = new ArrayList<String>();
+			
+			Scanner s = new Scanner(file);
+			while (s.hasNextLine()){
+				String line = s.nextLine();
+				String rule = line.split(" ")[0];
+				rulesList.add(rule);
+			}
+			s.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO: handle exception
+		}
+		
+	}
+
+	
+	private void takeCareOfWeights (){
+		
+	weightsList = new ArrayList<String>();
+	
+	for (int i = -5 ; i<6; i++) {
+		weightsList.add(String.valueOf(i));
+	}
+	
+	}
+	
+
+}
